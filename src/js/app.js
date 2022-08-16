18bafe73f3dfac0cb633fa51cd74eeb9336978bd
pixel-park <pixel-park@users.noreply.github.com>
2022-08-16 18:04:54 +0300

    some descriptions added to the page

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,19 @@
 import { Viewer } from './viewer.js';
-const adrs = ['https://plnup.com/configurator/', 'https://pixel-park.github.io/'];
+
+const genInfo = document.getElementById('general-info');
+
+const adrs = ['https://plnup.com/configurator/', 'https://github.com/pixel-park/box-in-the-room', 'https://pixel-park.github.io/', 'https://angry-goldberg-208d05.netlify.app/', 'https://stupendous-dusk-cc1d16.netlify.app/', 'https://jovial-shaw-084749.netlify.app/'];
 let index = 0;
-let direction = 1;
+
 const viewer = new Viewer({
     dom: ".viewer",
     cb: ()=>{
       window.open(adrs[index],'popup','width=840,height=600');
-      console.log('oppening ' + adrs[index])
-      if(index >= adrs.length - 1){
-        direction = -1;
-      }
-      if(index <= 0){
-        direction = 1
-      }
-      index += direction;
+      genInfo.innerText = `oppening  "${adrs[index]}"...  ${(adrs.length - 1 - index)} addresses are left.`
+      index = index >= adrs.length - 1 ? 0 : index + 1;
     }
   });
   viewer.run();
   viewer.render()
+
+  
