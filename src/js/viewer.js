b69437144a7ac159313758b0a4639a7cf2f2f4aa
pixel-park <pixel-park@users.noreply.github.com>
2022-08-23 01:49:28 +0300

    3d creen version implemented

--- src/js/viewer.js
+++ src/js/viewer.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { CSS3DRenderer, CSS3DObject } from 'three/examples/jsm/renderers/CSS3DRenderer.js';
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { gsap } from "gsap";
 
@@ -9,19 +10,19 @@ import refl from '../img/reflaction.jpg';
 import roomDeff from '../img/map.jpg';
 import sky from '../img/sky.jpg'
 
-let newEnvMap, exrCubeRenderTarget;
-
 class Viewer {
     constructor(options) {
       
       this.container = document.querySelector(options.dom);
       this.callBack = options.cb;
-  
+      this.newEnvMap = null;
+      this.exrCubeRenderTarget = null;
       this.scene = new THREE.Scene();
+      this.sceneB = new THREE.Scene();
       this.width = this.container.offsetWidth;
       this.height = this.container.offsetHeight;
       this.viewBoxCoordinates = this.container.getBoundingClientRect();
-      this.time = 0;
+      
       this.camera = new THREE.PerspectiveCamera(
         46,
         this.width / this.height,
@@ -31,14 +32,11 @@ class Viewer {
       
       this.renderer = new THREE.WebGLRenderer({
         antialias: true,
-        alpha: true,
       });
-      
-      this.push = false;
 
-      this.renderer.setSize(this.width, this.height);
-      this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
       
+      this.renderer.setSize(this.width, this.height);
+            
       this.container.appendChild(this.renderer.domElement);
   
       this.mouse = new THREE.Vector2();
@@ -46,60 +44,73 @@ class Viewer {
     }
   
     run(){
-      return new Promise((resolve)=>{
         this.watchResize();
-        this.loadEnvMap().then(()=>{
-          
-          this.setMaterials().then(()=>{
-            this.addRoom()
-            .then(()=>{
-            this.addSky();
-            this.watchers();
-            this.controls();
-            this.cameraPlacement();
-          })
-          })
-        })
-        
-        
-      })
-      
+        this.setotherRender();
+        this.loadEnvMap()
+        .then(this.setMaterials.bind(this))
+        .then(this.addRoom.bind(this))
+        .then(this.addSky.bind(this))
+        .then(()=>{
+          this.watchers();
+          this.replcae();
+          this.controls();
+          this.cameraPlacement();
+          this.createPlane(
+            1280 , 720,
+            new THREE.Vector3( -3.5, 1.5, 0 ),
+            new THREE.Euler( 0, 90 * THREE.MathUtils.DEG2RAD, 0 )
+          );
+          this.render();
+        })            
     }
   
-    setRender() {
-      this.renderer = new THREE.WebGLRenderer({
+    setotherRender() {
+      this.cssRenderer = new CSS3DRenderer();
+			this.cssRenderer.setSize( this.width, this.height );
+			this.cssRenderer.domElement.style.position = 'fixed';
+      this.cssRenderer.domElement.style.left = Math.round(this.viewBoxCoordinates.left) + 'px'
+     
+			this.container.appendChild( this.cssRenderer.domElement );
+
+      this.rendererB = new THREE.WebGLRenderer({
         antialias: true,
         alpha: true,
       });
-      this.rendererPhys.shadowMap.enabled = true;
-      this.rendererPhys.shadowMap.autoUpdate = false;
-      const shadowTypes = [THREE.BasicShadowMap, THREE.PCFShadowMap, THREE.PCFSoftShadowMap, THREE.VSMShadowMap]
-      this.rendererPhys.shadowMap.type = shadowTypes[1];
-      
-      this.rendererPhys.setSize(this.width, this.height);
-      this.rendererPhys.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-      
-      this.container.appendChild(this.rendererPhys.domElement);
+      this.rendererB.setSize(this.width, this.height);
+      this.rendererB.domElement.style.position = 'absolute';
+      this.rendererB.domElement.style.pointerEvents = 'none';
+      this.container.appendChild( this.rendererB.domElement );
     }
-  
+    
+    createPlane( width, height, pos, rot ) {
+      const element = document.createElement( 'div' );
+      element.style.width = width + 'px';
+      element.style.height = height + 'px';
+      element.style.opacity = 1;
+
+      const object = new CSS3DObject( element );
+      object.position.copy( pos );
+      object.rotation.copy( rot );
+      object.scale.set(1/333, 1/333, 1/333)
+      this.scene.add( object );
+    }
+
     loadEnvMap() {
       return new Promise((resolve)=>{
         THREE.DefaultLoadingManager.onLoad = function () {
           pmremGenerator.dispose();
       };
   
-      // -----------------
-  
-      const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
+      const pmremGenerator = new THREE.PMREMGenerator(this.rendererB);
       pmremGenerator.compileCubemapShader();
       new THREE.TextureLoader()
       .load(
         refl,
         (texture)=>{
-          exrCubeRenderTarget = pmremGenerator.fromEquirectangular(texture);
+          this.exrCubeRenderTarget = pmremGenerator.fromEquirectangular(texture);
           
-          newEnvMap = exrCubeRenderTarget ? exrCubeRenderTarget.texture : null;
-          resolve(newEnvMap)
+          this.newEnvMap = this.exrCubeRenderTarget ? this.exrCubeRenderTarget.texture : null;
+          resolve(this.newEnvMap)
   
           pmremGenerator.dispose()
           texture.dispose();
@@ -107,7 +118,7 @@ class Viewer {
         )
       })
       
-  }
+    }
   
     watchResize() {
       window.addEventListener("resize", this.resize.bind(this));
@@ -122,11 +133,10 @@ class Viewer {
     setMaterials() {
       return new Promise((resolve)=>{
         this.buttonPassive = new THREE.MeshBasicMaterial({
-          // reflectivity: 0.9,
           refractionRatio: 0.6,
           opacity: 0.9,
           transparent: true,
-          envMap: newEnvMap,
+          envMap: this.newEnvMap,
           color: '#2596be'
         })
         this.buttonHovered = new THREE.MeshBasicMaterial({
@@ -134,7 +144,7 @@ class Viewer {
           refractionRatio: 0.6,
           opacity: 0.8,
           transparent: true,
-          envMap: newEnvMap,
+          envMap: this.newEnvMap,
           color: "rgb(50, 255, 50)"
         })
           let texture = new THREE.TextureLoader().load(roomDeff)
@@ -142,9 +152,8 @@ class Viewer {
         this.frameMat = new THREE.MeshBasicMaterial({
           reflectivity: 0.2,
           opacity: 1,
-          // transparent: true,
           map: texture,
-          envMap: newEnvMap,
+          envMap: this.newEnvMap,
           color: 'white'
         })
         resolve()
@@ -156,9 +165,13 @@ class Viewer {
       this.width = this.container.offsetWidth;
       this.height = this.container.offsetHeight;
       this.renderer.setSize(this.width, this.height);
+      this.cssRenderer.setSize(this.width, this.height);
+      this.rendererB.setSize(this.width, this.height);
       this.camera.aspect = this.width / this.height;
       this.camera.updateProjectionMatrix();
       this.viewBoxCoordinates = this.container.getBoundingClientRect();
+      this.cssRenderer.domElement.style.left = Math.round(this.viewBoxCoordinates.left) + 'px';
+      this.cssRenderer.domElement.style.top = Math.round(this.viewBoxCoordinates.top) + 'px';
     }
 
     cameraPlacement() {
@@ -169,16 +182,44 @@ class Viewer {
       this.OrbitControl.update();
     }
   
+    cameraToOpen(){
+        const cam = gsap.timeline()
+        cam.to(this.camera.position,{
+        x: 0.9909573482408557,
+        y: 0.7690411865791031,
+        z: -0.02832073862241287,
+        duration: 1,
+        ease: 'power1.inOut',
+      });
+        cam.to(this.camera.rotation,{
+        x: 1.731800365477846,
+        y: 1.536151456479124,
+        z: -1.731895374273244,
+        duration: 2,
+        ease: 'power1.inOut',
+      }, '<');
+        cam.to(this.OrbitControl.target,{
+        x: 0,
+        y: 0.85,
+        z: 0,
+        duration: 2,
+        ease: 'power1.inOut',
+      }, '<');
+        this.OrbitControl.camera = this.camera;
+        this.OrbitControl.update();
+
+    }
+
     controls() {
       this.OrbitControl = new OrbitControls(
         this.camera,
-        this.renderer.domElement
+        this.cssRenderer.domElement
       ); 
       this.OrbitControl.maxPolarAngle = 100 * (Math.PI/180)  
       this.OrbitControl.minDistance = 0.5;
       this.OrbitControl.maxDistance = 2.6;
       this.OrbitControl.update();
-      }
+    }
   
     addRoom() {
       let loader = new GLTFLoader();
@@ -214,11 +255,20 @@ class Viewer {
           }
         );
         this.scene.add(object.scene);
-        // console.log(this.scene)
       })
     }
     )
-  }
+    }
+
+    replcae(){
+    const list = ['box', 'frame', 'button'];
+    list.forEach(item=>{
+      const target = this.scene.getObjectByName(item);
+      this.sceneB.add(target);
+      this.scene.remove(target)
+    })
+
+    }
 
     addSky(){
       let loader = new GLTFLoader();
@@ -237,7 +287,6 @@ class Viewer {
                 mesh.material = new THREE.MeshBasicMaterial({
                             map: texture,
                           })
-             
           },
           (xhr) => {
             console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
@@ -265,7 +314,6 @@ class Viewer {
         this.button[0].material = this.buttonPassive;
         this.container.style.cursor = 'auto';
       }
-  
     }
 
     mouseClick(evt){
@@ -276,8 +324,7 @@ class Viewer {
       let intersects = this.raycaster.intersectObjects(this.button);
       
       if (intersects.length > 0){
-        // this.button[0].material = this.buttonPassive;
-        
+              
         const push = gsap.timeline();
         push.to(this.button[0].position,{
           y: 0.57,
@@ -305,6 +352,8 @@ class Viewer {
 
     render() {
       this.renderer.render(this.scene, this.camera);
+      this.cssRenderer.render(this.scene, this.camera);
+      this.rendererB.render(this.sceneB, this.camera);
       this.requestFrame =  window.requestAnimationFrame(this.render.bind(this));
     }
   }
